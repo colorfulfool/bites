0ed598535a37f68e23e2e15c207f9bfbbdc82dcb
colorfulfool <t0ny.hamster@gmail.com>
2016-11-19 17:41:59 +0300

    Insert templates to make tests pass

--- test/controllers/results_controller_test.rb
+++ test/controllers/results_controller_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class ResultsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get results_new_url
+    xhr :get, new_experiment_result_path(experiments(:long_survey))
     assert_response :success
   end
-
 end
