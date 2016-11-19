18be69e05001a7254138398fbb0e2692b736bbc2
colorfulfool <t0ny.hamster@gmail.com>
2016-11-19 17:52:12 +0300

    Deal with deprecation warnings

--- test/controllers/results_controller_test.rb
+++ test/controllers/results_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ResultsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    xhr :get, new_experiment_result_path(experiments(:long_survey))
+    get new_experiment_result_path(experiments(:long_survey)), xhr: true
     assert_response :success
   end
 end
