0b2fe4dd9199c9f5f29c870711ccbba61774f887
colorfulfool <t0ny.hamster@gmail.com>
2016-11-26 15:08:42 +0300

    Make log-in page work

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -3,6 +3,11 @@ module Authentication
 
   included do
     helper_method :current_user
+    helper_method :logged_in?
+  end
+
+  def logged_in?
+    current_user.present?
   end
 
   def current_user
